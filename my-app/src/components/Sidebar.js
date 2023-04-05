c81120da0e955faba681c7d2d7cb9e12a84ad893
rubengardner <rgardnercuesta@gmail.com>
2023-04-05 17:00:55 +0000

    Add sidebar jsx

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Sidebar(){
+    return(
+        <div>
+         <div>
+            <a href = "#." target= "_blank">My Photos</a>
+            <a href = "#." target= "_blank">My Illustrations</a>
+            <a href = "#." target= "_blank">My Paintings</a>
+        </div>
+        </div>
+    )
+}
+// const Sidebar = () => <h1>It works</h1>
+
+export default Sidebar;
